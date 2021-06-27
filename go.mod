f6cd2740b2041dd98da2d1f1af692a02452c9cec
Brandon Sprague <brandon00sprague@gmail.com>
2021-06-27 13:56:10 -0700

    Start pulling things out of the main() function (#1)
    This PR starts abstracting out bits of functionality (loading users,
    generating keys) into their own functions, mostly for good practice but
    also theoretically so that they can be tested (in the future).
    
    Specifically, this PR does the following:
    - Introduces a `flag` package that we can use to take in parameters,
    which is needed for productionization
    - Persists our hash + block keys to files so that cookies can be valid
    across server runs
    - Pulls out loading users into a separate function
    - Allows the address to be configurable, also mainly for a server
    deployment
    
    We use the `namsral/flag` package instead of the standard library's
    `flag` package so that we can pass in configuration via environment
    variables and config files for extra flexibility.

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/lexacali/fivethreeone
 
 go 1.16
 
-require github.com/gorilla/securecookie v1.1.1 // indirect
+require (
+	github.com/gorilla/securecookie v1.1.1 // indirect
+	github.com/namsral/flag v1.7.4-pre // indirect
+)
--- main.go
+++ main.go
@@ -3,27 +3,34 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/gorilla/securecookie"
+	"github.com/namsral/flag"
 )
 
 func main() {
-	file, err := os.Open("users.json")
+	var (
+		usersFile    = flag.String("users_file", "users.json", "Path to the JSON file containing a mapping from password -> user name")
+		hashKeyFile  = flag.String("hash_key_file", "hashkey.dat", "Path to the file containing our secure cookie hash key")
+		blockKeyFile = flag.String("block_key_file", "blockkey.dat", "Path to the file containing our secure cookie block key")
+
+		addr = flag.String("addr", ":8080", "The address to run the HTTP server on")
+	)
+	flag.Parse()
+
+	users, err := loadUsers(*usersFile)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("failed to load users file: %v", err)
 	}
 
-	var users map[string]string
-	if err := json.NewDecoder(file).Decode(&users); err != nil {
-		log.Fatal(err)
+	s, err := loadSecureCookie(*hashKeyFile, *blockKeyFile)
+	if err != nil {
+		log.Fatalf("failed to load secure cookie: %v", err)
 	}
-
-	hashKey := securecookie.GenerateRandomKey(32)
-	blockKey := securecookie.GenerateRandomKey(32)
-	s := securecookie.New(hashKey, blockKey)
 
 	http.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != http.MethodPost {
@@ -79,5 +86,58 @@ func main() {
 		fmt.Fprintf(w, "The value of name is %q", value["name"])
 	})
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(*addr, nil))
+}
+
+func loadUsers(usersFile string) (map[string]string, error) {
+	f, err := os.Open(usersFile)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open users file: %w", err)
+	}
+	defer f.Close()
+
+	var users map[string]string
+	if err := json.NewDecoder(f).Decode(&users); err != nil {
+		return nil, fmt.Errorf("failed to parse users file as JSON: %w", err)
+	}
+	return users, nil
+}
+
+func loadSecureCookie(hashKeyFile, blockKeyFile string) (*securecookie.SecureCookie, error) {
+	hashKey, err := loadOrCreateKey(hashKeyFile)
+	if err != nil {
+		return nil, fmt.Errorf("failed to load or create hash key: %w", err)
+	}
+	blockKey, err := loadOrCreateKey(blockKeyFile)
+	if err != nil {
+		return nil, fmt.Errorf("failed to load or create block key: %w", err)
+	}
+	return securecookie.New(hashKey, blockKey), nil
+}
+
+func loadOrCreateKey(keyPath string) ([]byte, error) {
+	f, err := os.Open(keyPath)
+	if os.IsNotExist(err) {
+		return createKey(keyPath)
+	}
+	if err != nil {
+		return nil, fmt.Errorf("failed to load key from %q: %w", keyPath, err)
+	}
+	defer f.Close()
+
+	// If we're here, the key exists and we should load it.
+	dat, err := ioutil.ReadAll(f)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read existing key from %q: %w", keyPath, err)
+	}
+
+	return dat, nil
+}
+
+func createKey(keyPath string) ([]byte, error) {
+	dat := securecookie.GenerateRandomKey(32)
+	if err := ioutil.WriteFile(keyPath, dat, 0700); err != nil {
+		return nil, fmt.Errorf("failed to write key file %q: %w", keyPath, err)
+	}
+	return dat, nil
 }
